6bbc6a018f2083c227f62d2913af6951b7c267e2
ge26yim <ce.winkler@tum.de>
2022-12-15 14:08:49 +0100

    add puzz

--- day1/cw/main.py
+++ day1/cw/main.py
@@ -1,11 +1,11 @@
 # How much calories are carried by the elf carrying most cals? (here 24000)
 # TASK: Find elf carrying most calories. How many cals is it carrying?
-
+import pdb
 def func():
     sum = 0
     i = 0
     elveCalDict = {}
-    with open ('../puzzle1.txt') as f:
+    with open ('puzzle_cw.txt') as f:
         for char in f:
             if char == '\n':
                 elveCalDict[i] = sum
@@ -14,7 +14,6 @@ def func():
 
             else:
                 sum += int(char)
-
         key = max(elveCalDict, key=elveCalDict.get)
         max_cal = elveCalDict[key]
         print("Elve carrying most calories:",key,"\nCarrying {} calories.".format(max_cal))
